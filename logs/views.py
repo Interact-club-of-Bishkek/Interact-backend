ed57b7dd054d256f286a2755d72aacb558d0df84
Maruf995git <mamadalievmaruf740@gmail.com>
2025-11-29 21:56:24 +0600

    test logs

--- logs/views.py
+++ logs/views.py
@@ -3,11 +3,12 @@ from django.contrib.admin.models import LogEntry
 import json
 
 def logs_view(request):
-    entries = LogEntry.objects.order_by("-action_time")  # не select_related
+    entries = LogEntry.objects.order_by("-action_time")
 
     logs = []
 
     for entry in entries:
+        # обработка изменений
         changes = {}
         try:
             msg = json.loads(entry.change_message)
@@ -18,17 +19,20 @@ def logs_view(request):
         except:
             pass
 
+        # безопасное определение model_name
+        model_cls = entry.content_type.model_class() if entry.content_type else None
+        model_name = model_cls._meta.verbose_name.title() if model_cls else "—"
+
         logs.append({
             "id": entry.id,
             "user": entry.user if entry.user else "Неизвестно",
             "action_flag": entry.action_flag,
-            "model_name": entry.content_type.model_class()._meta.verbose_name.title()
-                          if entry.content_type else "—",
+            "model_name": model_name,
             "object_id": entry.object_id,
             "object_repr": entry.object_repr,
             "action_time": entry.action_time,
             "changes": changes,
-            "change_message": entry.change_message,  # для URL и времени
+            "change_message": entry.change_message,
         })
 
     return render(request, "logs/logs.html", {"logs": logs})
